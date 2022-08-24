b72075ce0fa8a7a770e15a44f165d528b5e1fb2a
gauravICT <44467412+gauravICT@users.noreply.github.com>
2022-08-24 22:13:44 +0530

    Create script.js
    adding script.js file.

---
+++ Day-1/script.js
@@ -0,0 +1,5 @@
+// Just the warmup for showing aleart on browser using some tiny logic
+let js='amazing';
+if (js === "amazing") alert("It's day 1!");
+// Just showing things on console
+console.log(12+34+90);
